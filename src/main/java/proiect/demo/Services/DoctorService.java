8406edce5a546bd5918409e9c9069f9f3a2feae1
MihaiNicoleta <100291123+MihaiNicoleta@users.noreply.github.com>
2023-06-08 01:08:14 +0300

    .

--- src/main/java/proiect/demo/Services/DoctorService.java
+++ src/main/java/proiect/demo/Services/DoctorService.java
@@ -3,7 +3,9 @@ package proiect.demo.Services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import proiect.demo.Domain.Doctor;
+import proiect.demo.Domain.User;
 import proiect.demo.Repostiories.DoctorRepository;
+import proiect.demo.Repostiories.UserRepository;
 import proiect.demo.configs.ResourceNotFoundException;
 
 import java.util.List;
@@ -13,42 +15,26 @@ public class DoctorService {
 
     @Autowired
     private DoctorRepository doctorRepository;
-    /**
-     * Returnează doctorul cu ID-ul specificat.
-     *
-     * @param id ID-ul doctorului.
-     * @return Doctorul cu ID-ul specificat.
-     * @throws ResourceNotFoundException dacă doctorul nu poate fi găsit.
-     */
+
+    @Autowired
+    private UserRepository userRepository;
+
     public Doctor getDoctorById(int id) {
         return doctorRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Doctor with id=" + id + " not found"));
     }
-    /**
-     * Returnează doctorul cu adresa de e-mail specificată.
-     *
-     * @param email Adresa de e-mail a doctorului.
-     * @return Doctorul cu adresa de e-mail specificată.
-     */
+
     public Doctor getDoctorByEmail(String email) {
         return doctorRepository.findByEmail(email);
     }
-    /**
-     * Returnează o listă cu toți doctorii.
-     *
-     * @return Lista cu toți doctorii.
-     */
+
     public List<Doctor> getAllDoctors() {
         return doctorRepository.findAll();
     }
 
-    /**
-     * Returnează o listă cu doctorii dintr-un anumit departament.
-     *
-     * @param departmentId ID-ul departamentului.
-     * @return Lista cu doctorii din departamentul specificat.
-     */
+
     public List<Doctor> getDoctorsByDepartment(Integer departmentId) {
         return doctorRepository.findByDepartmentId(departmentId);
     }
+
 }
